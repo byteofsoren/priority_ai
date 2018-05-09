6617164af4dbfbe3ccdea3de6c078e1e3f92a439
byteofsoren <r00tr4t@gmail.com>
2018-05-09 18:29:15 +0200

    Able to send text to robotspach

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,18 @@
 #include "blindy_findy/distances.h"
 #include "yolo_depth_fusion/yoloObject.h"
 #include "yolo_depth_fusion/yoloObjects.h"
+#include "robospeak/sayString.h"
 
-#define CHANNEL_DELAY 1000
+
+
+#define CHANNEL_DELAY 1
 #define FINDY_FTOI(x) (int) 10*x
 
 
 Sound global_tones;
 std::string global_path;
 void blindy_findy_callback(const blindy_findy::distances msg);
+void say(std::string text);
 
 int main(int argc, char *argv[])
 {
@@ -39,7 +43,18 @@ int main(int argc, char *argv[])
    global_tones.add_sound(global_path + "/resources/sonar.wav","sonar");
    global_tones.add_sound(global_path + "/resources/tom.wav","tom");
    ros::Subscriber sub_blindy = rnode.subscribe("/distances",1000,blindy_findy_callback);
-   ros::spin();
+   ros::ServiceClient robclient = rnode.serviceClient<robospeak::sayString>("say_string");
+   robospeak::sayString srv;
+   ros::Rate loop_rate(20);
+   while(ros::ok()){
+      srv.request.str = "Hello from priority ai";
+      ROS_INFO_STREAM("Sending: " << srv.request.str << "\n");
+      if(robclient.call(srv))
+         ROS_INFO_STREAM("Return: " << srv.response.str << "\n");
+      ros::spinOnce();
+      loop_rate.sleep();
+   }
+
 
    return 0;
 }
@@ -52,7 +67,7 @@ void blindy_findy_callback(const blindy_findy::distances msg){
    int iright = FINDY_FTOI(right);
    int imid = FINDY_FTOI(mid);
    int ileft = FINDY_FTOI(left);
-   
+   //ROS_INFO_STREAM("counter=" << counter << "st " << counter%(CHANNEL_DELAY * iright) << "\n");
    usleep(10);
    if (counter%(CHANNEL_DELAY * iright)== 0){
       ROS_INFO("Right");
@@ -66,6 +81,9 @@ void blindy_findy_callback(const blindy_findy::distances msg){
       ROS_INFO("Left");
       global_tones.play_sound("tom",180-50,left);
    }
-   //ROS_INFO_STREAM("left " << left << " mid " << mid << " right " << right << "\n");
    counter++;
+}
+
+void say(std::string text){
+
 }
